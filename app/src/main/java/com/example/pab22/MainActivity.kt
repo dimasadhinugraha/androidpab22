e3d6b2f86aec6867f01e07fbf24b1fe41383481e
dimasadhinugraha <dimasadhin@gmail.com>
2025-10-08 12:48:54 +0700

    update

--- app/src/main/java/com/example/pab22/MainActivity.kt
+++ app/src/main/java/com/example/pab22/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.pab22
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -9,5 +12,17 @@ class MainActivity : AppCompatActivity() {
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val inpnama = findViewById<EditText>(R.id.input_nama)
+        val btnsbmt = findViewById<Button>(R.id.btn_submit)
+
+        btnsbmt.setOnClickListener {
+            val nama = inpnama.text.toString()
+            if (nama.isEmpty()){
+                Toast.makeText(this, "Nama belum diisi!", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Halo, $nama!!", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
