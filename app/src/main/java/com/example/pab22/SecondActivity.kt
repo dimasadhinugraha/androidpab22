76ff4f4bdb35da04a9071106ea089ab9dbd946df
dimasadhinugraha <dimasadhin@gmail.com>
2025-10-22 12:41:10 +0700

    add

--- app/src/main/java/com/example/pab22/SecondActivity.kt
+++ app/src/main/java/com/example/pab22/SecondActivity.kt
@@ -1,12 +1,34 @@
 package com.example.pab22
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+
 
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        val txtResult = findViewById<TextView>(R.id.txtResult)
+        val url = "https://jsonplaceholder.typicode.com/users"
+        val queue = Volley.newRequestQueue(this)
+
+        val request = StringRequest(Request.Method.GET, url, {
+                response ->
+            txtResult.text = response.substring(0, 100) + ".."
+        },
+        {
+            error ->
+            txtResult.text = "Gagal Memuat Data: ${error.message}"
+        }
+        )
+        queue.add(request)
+
     }
 }
